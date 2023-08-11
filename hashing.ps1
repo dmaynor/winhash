1f39688e6d8c36c8368f8453bc4e16263f2f9752
David Maynor <dmaynor@users.noreply.github.com>
2023-08-11 18:09:04 -0400

    Create hashing.ps1
    Add original hash script.

---
+++ hashing.ps1
@@ -0,0 +1,50 @@
+# Define the output file name format
+$timestamp = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+$outputFile = "hashrun-$timestamp.csv"
+
+# Function to compute hash values for a file and write to CSV
+function ComputeHashForFile($file) {
+    try {
+        # Getting file hashes
+        $md5 = (Get-FileHash -Path $file.FullName -Algorithm MD5).Hash
+        $sha1 = (Get-FileHash -Path $file.FullName -Algorithm SHA1).Hash
+        $sha256 = (Get-FileHash -Path $file.FullName -Algorithm SHA256).Hash
+
+        # Constructing the result object
+        $result = [PSCustomObject]@{
+            'FileName'      = $file.Name
+            'FullPath'      = $file.FullName
+            'MD5'           = $md5
+            'SHA1'          = $sha1
+            'SHA256'        = $sha256
+            'CreationDate'  = $file.CreationTime
+            'AccessDate'    = $file.LastAccessTime
+            'ModifiedDate'  = $file.LastWriteTime
+        }
+
+        # Append the result to CSV file
+        $result | Export-Csv -Path $outputFile -NoTypeInformation -Append
+
+    } catch {
+        Write-Error "Failed to process file: $($file.FullName). Error: $($_.Exception.Message)"
+    }
+}
+
+# Create CSV header by exporting an empty object (this will ensure headers are written)
+[PSCustomObject]@{
+    'FileName'      = $null
+    'FullPath'      = $null
+    'MD5'           = $null
+    'SHA1'          = $null
+    'SHA256'        = $null
+    'CreationDate'  = $null
+    'AccessDate'    = $null
+    'ModifiedDate'  = $null
+} | Export-Csv -Path $outputFile -NoTypeInformation
+
+# Recursively find all files starting from C:\ and compute their hash values
+Get-ChildItem -Path C:\ -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object {
+    ComputeHashForFile $_
+}
+
+Write-Output "Hash computation completed. Results saved to $outputFile"
